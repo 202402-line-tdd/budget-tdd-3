01058d4b51750c6a0815e8bf0a6c11c70795572e
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:55:02 +0800

    clean up and rename variable: data to filterBudgets

--- index.js
+++ index.js
@@ -5,52 +5,44 @@ const isBetween = require("dayjs/plugin/isBetween");
 dayjs.extend(isBetween);
 
 class BudgetService {
-  query(start, end) {
-    const startDate = dayjs(start);
-    const endDate = dayjs(end);
-    // start > end
-    if (startDate.isAfter(endDate)) {
-      return 0;
+    query(start, end) {
+        const startDate = dayjs(start);
+        const endDate = dayjs(end);
+        if (startDate.isAfter(endDate)) {
+            return 0;
+        }
+
+        const startMonth = startDate.format("YYYYMM");
+        const endMonth = endDate.format("YYYYMM");
+        const startMonthDays = startDate.daysInMonth();
+
+        const filterBudgets = this.getAll().filter((budget) => {
+            const yearMonth = dayjs(budget.yearMonth);
+            return yearMonth.isBetween(startDate, endDate, "month", "[]");
+        });
+
+        return filterBudgets
+            .map((budget) => {
+                const daysInMonth = dayjs(budget.yearMonth).daysInMonth();
+
+                if (startMonth === endMonth) {
+                    return (budget.amount / daysInMonth) * (endDate.diff(startDate, "days") + 1);
+                }
+
+                if (budget.yearMonth === startMonth) {
+                    const startDays = startMonthDays - startDate.get("date") + 1;
+                    return (budget.amount / daysInMonth) * startDays;
+                } else if (budget.yearMonth === endMonth) {
+                    return (budget.amount / daysInMonth) * endDate.get("date");
+                } else {
+                    return budget.amount;
+                }
+            })
+            .reduce((sum, current) => (sum + current), 0);
     }
 
-    const startMonth = startDate.format("YYYYMM");
-    const endMonth = endDate.format("YYYYMM");
-    const startMonthDays = startDate.daysInMonth();
-
-    const data = this.getAll().filter((budget) => {
-      const yearMonth = dayjs(budget.yearMonth);
-
-      return yearMonth.isBetween(startDate, endDate, "month", "[]");
-    });
-
-    return data
-      .map((budget) => {
-        const daysInMonth = dayjs(budget.yearMonth).daysInMonth();
-
-        // same month
-        if (startMonth === endMonth) {
-          return (
-            (budget.amount / daysInMonth) *
-            (endDate.diff(startDate, "days") + 1)
-          );
-        }
-
-        // cross moth
-        if (budget.yearMonth === startMonth) {
-          const startDays = startMonthDays - startDate.get("date") + 1;
-          return (budget.amount / daysInMonth) * startDays;
-        } else if (budget.yearMonth === endMonth) {
-          return (budget.amount / daysInMonth) * endDate.get("date");
-        } else {
-          return budget.amount;
-        }
-      })
-      .reduce((sum, current) => {
-        return (sum += current);
-      }, 0);
-  }
-
-  getAll() {}
+    getAll() {
+    }
 }
 
 module.exports = BudgetService;
