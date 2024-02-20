a2d1b693545875ce83d8f4fa5768dbc1536d462a
scott.hsieh <scott.hsieh@linecorp.com>
2024-02-20 17:40:24 +0800

    complete function and test

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log('Hello world');
+const dayjs = require("dayjs");
+
+const isBetween = require("dayjs/plugin/isBetween");
+
+dayjs.extend(isBetween);
+
+class BudgetService {
+  query(start, end) {
+    const startDate = dayjs(start);
+    const endDate = dayjs(end);
+    // start > end
+    if (startDate.isAfter(endDate)) {
+      return 0;
+    }
+
+    const startMonth = startDate.format("YYYYMM");
+    const endMonth = endDate.format("YYYYMM");
+    const startMonthDays = startDate.daysInMonth();
+
+    const data = this.getAll().filter((budget) => {
+      const yearMonth = dayjs(budget.yearMonth);
+
+      return yearMonth.isBetween(startDate, endDate, "month", "[]");
+    });
+
+    return data
+      .map((budget) => {
+        const daysInMonth = dayjs(budget.yearMonth).daysInMonth();
+
+        // same month
+        if (startMonth === endMonth) {
+          return (
+            (budget.amount / daysInMonth) *
+            (endDate.diff(startDate, "days") + 1)
+          );
+        }
+
+        // cross moth
+        if (budget.yearMonth === startMonth) {
+          const startDays = startMonthDays - startDate.get("date") + 1;
+          return (budget.amount / daysInMonth) * startDays;
+        } else if (budget.yearMonth === endMonth) {
+          return (budget.amount / daysInMonth) * endDate.get("date");
+        } else {
+          return budget.amount;
+        }
+      })
+      .reduce((sum, current) => {
+        return (sum += current);
+      }, 0);
+  }
+
+  getAll() {}
+}
+
+module.exports = BudgetService;
---
+++ index.test.js
@@ -0,0 +1,78 @@
+const BudgetService = require("./index");
+
+class Budget {
+  constructor(yearMonth, amount) {
+    this.yearMonth = yearMonth;
+    this.amount = amount;
+  }
+}
+
+describe("", () => {
+  const budgetService = new BudgetService();
+
+  it("invalid", () => {
+    budgetService.getAll = () => {
+      return [
+        new Budget("202312", 31),
+        new Budget("202401", 310),
+        new Budget("202402", 29),
+        new Budget("202403", 620),
+        new Budget("202405", 600),
+      ];
+    };
+    const result = budgetService.query("2024-01-10", "2024-01-01");
+    expect(result).toBe(0);
+  });
+
+  it("query one day", () => {
+    budgetService.getAll = () => {
+      return [new Budget("202401", 620)];
+    };
+    const result = budgetService.query("2024-01-01", "2024-01-01");
+    expect(result).toBe(20);
+  });
+
+  it("start end in same month ", () => {
+    budgetService.getAll = () => {
+      return [new Budget("202401", 620)];
+    };
+    const result = budgetService.query("2024-01-01", "2024-01-10");
+    expect(result).toBe(200);
+  });
+
+  it("cross month", () => {
+    budgetService.getAll = () => {
+      return [new Budget("202401", 310), new Budget("202402", 29)];
+    };
+    const result = budgetService.query("2024-01-29", "2024-02-02");
+    expect(result).toBe(32);
+  });
+
+  it("null data month", () => {
+    budgetService.getAll = () => {
+      return [new Budget("202401", 310), new Budget("202402", 29)];
+    };
+    const result = budgetService.query("2024-03-29", "2024-03-31");
+    expect(result).toBe(0);
+  });
+
+  it("cross null data month", () => {
+    budgetService.getAll = () => {
+      return [new Budget("202401", 310), new Budget("202403", 620)];
+    };
+    const result = budgetService.query("2024-01-29", "2024-03-31");
+    expect(result).toBe(650);
+  });
+
+  it("cross year", () => {
+    budgetService.getAll = () => {
+      return [
+        new Budget("202312", 31),
+        new Budget("202401", 310),
+        new Budget("202403", 620),
+      ];
+    };
+    const result = budgetService.query("2023-12-30", "2024-03-30");
+    expect(result).toBe(912);
+  });
+});
